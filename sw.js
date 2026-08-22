1c33db866a010b547c3aa4e394667801030ba45b
Darlin Arenas <darlin699@gmil.com>
2026-08-22 00:27:52 -0400

    Revert "actualizacion mapa"
    This reverts commit 635c3e753ae57ab9d471bf3a7ed2f65cab114f16.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.22-mapa3d-localizador-v2';
+const APP_VERSION = '2026.08.22-mapa3d-operativo-v1';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
