46d15ce7f8bb80beb27c2861d5eac40862b4eb6b
Darlin Arenas <darlin699@gmil.com>
2026-08-22 23:54:23 -0400

    aceptar y asignar orden

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.22-flujo-orden-v4';
+const APP_VERSION = '2026.08.22-flujo-comprobado-v5';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
