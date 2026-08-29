869e8aabfd98d54456d8507fe0e79278f99e71de
Darlin Arenas <darlin699@gmil.com>
2026-08-29 12:46:30 -0400

    corregir acceso de Ricardo a Ordenes Mis tareas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.29-fix-entrada-ordenes-v30';
+const APP_VERSION = '2026.08.29-fix-entrada-ordenes-admin-v31';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
