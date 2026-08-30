9fd33bc5e3ad4b621026dcd8d720714595f7cb2c
Darlin Arenas <darlin699@gmil.com>
2026-08-30 18:39:56 -0400

    mejora diseño y marca de etiquetas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.30-etiquetas-posiciones-medidas-v57';
+const APP_VERSION = '2026.08.30-etiquetas-diseno-marca-v58';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
