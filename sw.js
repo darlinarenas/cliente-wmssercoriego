8d8dea35ea70ebc4fbe171cc44d6638e67ca052b
Darlin Arenas <darlin699@gmil.com>
2026-08-30 18:20:50 -0400

    evita duplicados en impresion de etiquetas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.30-etiquetas-rack-posiciones-v63';
+const APP_VERSION = '2026.08.30-etiquetas-antiduplicados-v64';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
