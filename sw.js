0a60d47e25fda30fbec411153b87201154252a90
Darlin Arenas <darlin699@gmil.com>
2026-08-30 18:45:15 -0400

    agrega seleccion de etiquetas desde mapa de racks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.30-etiquetas-diseno-marca-v58';
+const APP_VERSION = '2026.08.30-etiquetas-mapa-racks-v59';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
