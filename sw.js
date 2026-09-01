d4c6396859ebc8a68b2468b826a6b6f2fe4b1998
Darlin Arenas <darlin699@gmil.com>
2026-08-31 23:24:07 -0400

    simplificar levantamiento fisico para operador movil

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.31-inventario-rack-acceso-v66';
+const APP_VERSION = '2026.09.01-inventario-operador-compacto-v67';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
