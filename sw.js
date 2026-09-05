d216f5b90018739703098ef5862d898fdeb46edd
Darlin Arenas <darlin699@gmil.com>
2026-09-05 14:39:24 -0400

    compacta flujo movil de impresion de etiquetas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.05-android-zebra-v93';
+const APP_VERSION = '2026.09.05-labels-mobile-v94';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
