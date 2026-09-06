4acd2621ac5deb533272df06cb1f5144a582868a
Darlin Arenas <darlin699@gmil.com>
2026-09-05 22:58:31 -0400

    mejora inicio operario y picking inteligente

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.05-physical-stock-visual-v106';
+const APP_VERSION = '2026.09.05-operator-guided-picking-v107';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
