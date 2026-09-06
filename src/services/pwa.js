ef22dc2533ad5dbe9ae93332644cc75ddd3d4205
Darlin Arenas <darlin699@gmil.com>
2026-09-06 19:15:04 -0300

    corregir retiro externo para no dejar stock en transito

--- src/services/pwa.js
+++ src/services/pwa.js
@@ -1,4 +1,4 @@
-const PWA_RELEASE = '2026.09.05-install-visible-v95';
+const PWA_RELEASE = '2026.09.06-external-pickup-v112';
 const WMS_CACHE_PREFIX = 'sercoriego-lite-wms-';
 let eventoInstalacion = null;
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.06-inventory-compact-v111';
+const APP_VERSION = '2026.09.06-external-pickup-v112';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
