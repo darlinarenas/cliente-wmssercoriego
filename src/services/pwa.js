103ef8062e182cfd5f655d591e40873c5c1b0b58
Darlin Arenas <darlin699@gmil.com>
2026-09-06 21:13:58 -0300

    compactar globalmente las tareas del operario

--- src/services/pwa.js
+++ src/services/pwa.js
@@ -1,4 +1,4 @@
-const PWA_RELEASE = '2026.09.06-operator-inputs-v114';
+const PWA_RELEASE = '2026.09.06-operator-compact-v115';
 const WMS_CACHE_PREFIX = 'sercoriego-lite-wms-';
 let eventoInstalacion = null;
 
