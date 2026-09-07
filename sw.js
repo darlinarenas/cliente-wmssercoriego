9301cf36d3df32571f6d0d50d4c7e321a55fd0ab
Darlin Arenas <darlin699@gmil.com>
2026-09-07 13:11:59 -0300

    eliminar encabezados de impresion y priorizar Zebra directa

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.07-calibracion-impresion-zebra-v126';
+const APP_VERSION = '2026.09.07-impresion-limpia-zebra-v127';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
