198fb1b6bb08e1c444777bba6b623b4585bf8eb0
Darlin Arenas <darlin699@gmil.com>
2026-09-06 22:01:14 -0300

    ampliar ventana de gestion de usuarios

--- src/services/pwa.js
+++ src/services/pwa.js
@@ -1,4 +1,4 @@
-const PWA_RELEASE = '2026.09.06-pallet-permissions-v117';
+const PWA_RELEASE = '2026.09.06-user-dialog-wide-v118';
 const WMS_CACHE_PREFIX = 'sercoriego-lite-wms-';
 let eventoInstalacion = null;
 
