914787f5d97eacab1208c8e927d2e5257300a47e
Darlin Arenas <darlin699@gmil.com>
2026-09-08 20:45:17 -0300

    ajustar calibracion y formatos de impresion Zebra

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.08-orden-clientes-v129';
+const APP_VERSION = '2026.09.08-etiquetas-calibracion-v130';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
