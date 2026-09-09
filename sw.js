735edafd09f0f6ebb778546423abda69b8e8bf1f
Darlin Arenas <darlin699@gmil.com>
2026-09-08 22:17:02 -0300

    corregir medida rack y posicion a 100x30 mm

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.08-restaurar-agregar-etiquetas-v139';
+const APP_VERSION = '2026.09.08-rack-posicion-100x30-v140';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
