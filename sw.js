6bb7b82cf54ebf37a82036928d1af1767967074f
Darlin Arenas <darlin699@gmil.com>
2026-09-09 20:45:00 -0300

    volver impresion de etiquetas a PDF y limpiar interfaz

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.09-etiquetas-zebra-directa-v148';
+const APP_VERSION = '2026.09.09-etiquetas-pdf-v149';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
