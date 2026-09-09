dc6df9fe706e74f4d5de9bcf0022ece11f738c24
Darlin Arenas <darlin699@gmil.com>
2026-09-08 22:10:25 -0300

    restaurar botones agregar en etiquetas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.08-impresion-unificada-v138';
+const APP_VERSION = '2026.09.08-restaurar-agregar-etiquetas-v139';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
