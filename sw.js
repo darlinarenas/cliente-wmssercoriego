b1ba108a71a8671e92cbbe746f60508284c9b079
Darlin Arenas <darlin699@gmil.com>
2026-09-08 23:10:38 -0300

    corregir guardado permiso codigos con cantidad

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.09-codigos-cantidades-v142';
+const APP_VERSION = '2026.09.09-permisos-codigos-cantidades-v143';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
