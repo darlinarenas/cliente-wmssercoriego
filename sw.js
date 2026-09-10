e38be3bf4171ae890175306f9811f2e5ca1013d7
Darlin Arenas <darlin699@gmil.com>
2026-09-09 22:28:38 -0300

    mostrar y aplicar oscuridad Zebra en etiquetas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.09-oscuridad-zebra-v156';
+const APP_VERSION = '2026.09.09-oscuridad-zebra-visible-v157';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
