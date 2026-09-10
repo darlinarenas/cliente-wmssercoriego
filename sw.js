f9604d4c3119b76123592828f232b017829d12e0
Darlin Arenas <darlin699@gmil.com>
2026-09-09 22:58:05 -0300

    agregar ayuda visual de empaque en picking operador

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.09-oscuridad-zebra-visible-v157';
+const APP_VERSION = '2026.09.10-ayuda-empaque-picking-v158';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
