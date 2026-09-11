cdc8eb9263406164c789d1ee1aecba1284e2f1d5
Darlin Arenas <darlin699@gmil.com>
2026-09-11 11:34:30 -0300

    mostrar puente y ampliar vista de impresion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.10-salida-popup-cola-v180';
+const APP_VERSION = '2026.09.11-puente-vista-ampliable-v181';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
