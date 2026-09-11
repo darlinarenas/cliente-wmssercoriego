6aad0f000bd512d0e1f92d322ccfa449ac1a0cb7
Darlin Arenas <darlin699@gmil.com>
2026-09-10 22:04:44 -0300

    mejorar codigo manual y vista compacta de pallet

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.10-operator-manual-direct-v173';
+const APP_VERSION = '2026.09.10-manual-pallet-compact-v174';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
