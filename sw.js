a70a01de37839bdd5ef4f6ac2fa269e7cbb4b630
Darlin Arenas <darlin699@gmil.com>
2026-09-10 21:42:13 -0300

    mejorar controles y secciones compactas de etiquetas operador

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.10-operator-labels-compact-v170';
+const APP_VERSION = '2026.09.10-operator-labels-accordion-v171';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
