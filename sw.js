99024b9526f2f1f368a1b4a38ec5835f746582dc
Darlin Arenas <darlin699@gmil.com>
2026-09-11 18:14:01 -0300

    corregir vista adaptativa y activar PC puente de impresion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.11-preview-escritorio-v183';
+const APP_VERSION = '2026.09.11-impresion-adaptativa-puente-v184';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
