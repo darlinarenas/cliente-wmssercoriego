ca1595f7c5f8ca2d352deb830e3ed373864a26d1
Darlin Arenas <darlin699@gmil.com>
2026-09-11 11:41:39 -0300

    corregir vista previa de impresion en escritorio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.11-preview-premium-v182';
+const APP_VERSION = '2026.09.11-preview-escritorio-v183';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
