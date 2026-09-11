dda4cdd6bb4e045a0f865d882564f1e32d7722cd
Darlin Arenas <darlin699@gmil.com>
2026-09-11 19:46:40 -0300

    compactar y guiar organizar pallets operario v186

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.11-etiquetas-operario-v185';
+const APP_VERSION = '2026.09.11-organizar-pallet-v186';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
