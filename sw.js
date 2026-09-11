1ee51af7bfe7b0d11e16954551bcf4b7d78e4c25
Darlin Arenas <darlin699@gmil.com>
2026-09-10 22:53:58 -0300

    restaurar popup de cantidad y mejorar cola de salida

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.10-cola-mobile-visible-v179';
+const APP_VERSION = '2026.09.10-salida-popup-cola-v180';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
