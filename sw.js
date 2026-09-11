b5c1e6218b80145e4e7340d1b63258a011c79184
Darlin Arenas <darlin699@gmil.com>
2026-09-10 22:21:22 -0300

    agregar revision y limpieza de cola de etiquetas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.10-etiqueta-salida-pedido-v175';
+const APP_VERSION = '2026.09.10-cola-etiquetas-revisable-v176';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
