5b95c6c3006c7a71029a51aaca2563a2789dbb20
Darlin Arenas <darlin699@gmil.com>
2026-09-12 19:43:00 -0300

    corregir impresion remota del operador v198

--- src/services/pwa.js
+++ src/services/pwa.js
@@ -1,4 +1,4 @@
-const PWA_RELEASE = '2026.09.12-operador-solo-impresion-v197';
+const PWA_RELEASE = '2026.09.12-operador-puente-remoto-v198';
 const WMS_CACHE_PREFIX = 'sercoriego-lite-wms-';
 let eventoInstalacion = null;
 
