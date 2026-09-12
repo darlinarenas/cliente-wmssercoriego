8ba006cfcbdd29bd03aa38509a6211ec22f89616
Darlin Arenas <darlin699@gmil.com>
2026-09-12 19:28:03 -0300

    restaurar impresion local y nombre de puente v196

--- src/services/pwa.js
+++ src/services/pwa.js
@@ -1,4 +1,4 @@
-const PWA_RELEASE = '2026.09.12-panel-lateral-impresion-v195';
+const PWA_RELEASE = '2026.09.12-puente-nombrado-impresion-local-v196';
 const WMS_CACHE_PREFIX = 'sercoriego-lite-wms-';
 let eventoInstalacion = null;
 
