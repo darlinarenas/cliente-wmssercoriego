1102156ba712d4ca111dbc8cea7582e5773f9e4d
Darlin Arenas <darlin699@gmil.com>
2026-09-24 20:32:52 -0300

    Mejora picking operador con popups amplios y control de faltantes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.24-operator-compact-v218';
+const APP_VERSION = '2026.09.24-operator-popup-v219';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
