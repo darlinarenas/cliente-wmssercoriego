edae4a9dc91f5ba7fd8d2a52042f1b93bf78b20e
Darlin Arenas <darlin699@gmil.com>
2026-09-24 20:43:25 -0300

    Mejora legibilidad de descripcion SKU y ubicacion en picking operador

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.24-operator-popup-v219';
+const APP_VERSION = '2026.09.24-operator-popup-v220';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
