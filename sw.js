a5aa9066a20dedba177c2e725312a52299c44b48
Darlin Arenas <darlin699@gmil.com>
2026-09-24 22:14:55 -0300

    Corrige clic de guardar cantidad preparada en revision final

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.24-manager-correction-save-v225';
+const APP_VERSION = '2026.09.24-manager-correction-click-v226';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
