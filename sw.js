b16e6c87ec1fd56b2570b439da3281b28b1a95ed
Darlin Arenas <darlin699@gmil.com>
2026-09-24 21:21:40 -0300

    Agrega PDF y Excel de preparacion para jefe de bodega

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.24-operator-popup-v220';
+const APP_VERSION = '2026.09.24-picking-support-v221';
 const CACHE = `sercoriego-lite-wms-${APP_VERSION}`;
 const CACHE_PREFIX = 'sercoriego-lite-wms-';
 
